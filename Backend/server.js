b8f9914ee5a17028874f1b4844b1ea6ef11239af
rajesh <rajesh.swain@innoitlabs.com>
2025-08-16 12:11:50 +0530

    changes

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,11 @@ const dotenv=require("dotenv")
 dotenv.config()
 const app=express()
 
+app.get("/", (req, res) => {
+    console.log("server running.....");
+    res.send("Hello from Express!");
+});
+
 app.listen(process.env.PORT,()=>{
     console.log(`connected on port ${process.env.PORT}`)
 })
